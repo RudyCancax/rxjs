ef0ec6988601c3cbe598fe7bc280c78803951f6f
RudyCancax <rudycancax@gmail>
2022-12-10 12:28:35 -0600

    Feat: Observable and subscription to observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
+import { Observable } from "rxjs";
+
+// This way  isn't used any more, for declare an observable
+// const obs$ = Observable.create();
+
+// The correct way for declare an Observable 
+const obs$ = new Observable<string>( sub => {
+    sub.next('Hello');
+    console.log('1');
+    sub.next('World');
+    console.log('2');
+    sub.next('Hello');
+    console.log('3');
+    sub.next('World');
+    console.log('4');
+    sub.complete();    // The code after "complete" is not sended to the subscribers.
+    console.log('c');
+    sub.next('Hello');
+    console.log('1');
+    sub.next('World');
+    console.log('2');
+});
 
 
-console.log('Hola Mundo!');
-
-
-
-
-
-
-
+// Subscriber to the Observable -> We can use the console.log if we receive a paramether and just print
+// Instead of> obs$.subscribe( data => console.log(data))
+obs$.subscribe( console.log );
